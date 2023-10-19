c39fc29b66e93d0465ad5884e6cbb59ed3324917
chriskerrc <chriskerrc@gmail.com>
2023-10-19 18:51:06 +0100

    Working and tests finished
    Next step: apply style guide

--- ms.c
+++ ms.c
@@ -5,9 +5,9 @@
 int num_char(unsigned width, unsigned height, unsigned len);
 int char_set(unsigned len, char inp[MAXSQ*MAXSQ+1]);
 int num_mines_str(unsigned len, unsigned totmines, char inp[MAXSQ*MAXSQ+1]);
-unsigned num_mines_b(unsigned width, unsigned height, board b);
-board moore_count_rule_1(unsigned width, unsigned height, char position, char find, board b); //rename function - returns board
-unsigned moore_count_rule_2(unsigned width, unsigned height, unsigned row, unsigned col, char find, board b); //returns count 
+unsigned num_mines_board(unsigned width, unsigned height, board b);
+board rule_1_board(unsigned width, unsigned height, char cell_val, char find, board b); 
+unsigned moore_count(unsigned width, unsigned height, unsigned row, unsigned col, char find, board b);
 board apply_rule_2(unsigned width, unsigned height, unsigned row, unsigned col, board b);
 
 board solve_board(board b)
@@ -21,17 +21,16 @@ unsigned totmines = b.totmines;
       for(unsigned col = 0; col < width; col++){ 
          if(b.grid[row][col] != MINE && b.grid[row][col] != UNK){ //i.e. if it's a number 
             unsigned num = b.grid[row][col];
-            unsigned unk_count = moore_count_rule_2(width, height, row, col, UNK, b);
-            unsigned mine_count = moore_count_rule_2(width, height, row, col, MINE, b);
-            //printf("num %c, unk_count %u, mine_count %u\n", num, unk_count, mine_count);
-               if(num - '0' == unk_count + mine_count){ // - '0' to convert from ASCII value to number
+            unsigned unk_count = moore_count(width, height, row, col, UNK, b);
+            unsigned mine_count = moore_count(width, height, row, col, MINE, b);
+               if(num - '0' == unk_count + mine_count){ // - '0' to convert from ASCII value to number - typedef this const
                   b = apply_rule_2(width, height, row, col, b);   
                } 
            }
        }
     }
 
-//temp print function
+//temp print function - delete
 for(unsigned row = 0; row < height; row++){    
       for(unsigned col = 0; col < width; col++){ 
          printf("%c", b.grid[row][col]);
@@ -43,13 +42,13 @@ for(unsigned row = 0; row < height; row++){
 
 //rule 1 
 
-   if(num_mines_b(width, height, b)==totmines){
+   if(num_mines_board(width, height, b)==totmines){
       printf("Applying rule 1...\n");
-      b = moore_count_rule_1(width, height, UNK, MINE, b);
+      b = rule_1_board(width, height, UNK, MINE, b);
    }
 
 
-//temp print function
+//temp print function - delete
 for(unsigned row = 0; row < height; row++){    
       for(unsigned col = 0; col < width; col++){ 
          printf("%c", b.grid[row][col]);
@@ -92,8 +91,9 @@ else{
 
 }
 
-board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1]) /* split conversion from 1D string to 2D array into a separate function, then assert test it with expected values */
-{ //use unsigned int instead of int
+board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1])
+{ 
+//use unsigned int instead of int
 
 board b; 
 b.w = width; 
@@ -167,7 +167,7 @@ int num_mines_str(unsigned len, unsigned totmines, char inp[MAXSQ*MAXSQ+1])
    }
 }
 
-unsigned num_mines_b(unsigned width, unsigned height, board b) //compares mines in board with totmines
+unsigned num_mines_board(unsigned width, unsigned height, board b) //compares mines in board with totmines
 {
 unsigned cnt = 0;
 
@@ -178,32 +178,18 @@ unsigned cnt = 0;
          }
       }
    } 
-
 return cnt;
 }
 
-board moore_count_rule_1(unsigned width, unsigned height, char position, char find, board b) 
+board rule_1_board(unsigned width, unsigned height, char cell_val, char find, board b) 
 {
 
 int cnt= 0;
 for(unsigned row = 0; row < height; row++){    
          for(unsigned col = 0; col < width; col++){ 
-            if(b.grid[row][col] == position){
-               unsigned row_unk = row;
-               unsigned col_unk = col;
-                    for(int j = row-1; j < (int)row + 2 ; ++j){
-                        for(int i = col-1; i < (int)col + 2; ++i){
-                              if(i >= 0 && j >= 0 && i< (int)width && j<(int)height){
-                                 if (!((i == (int) col) && (j == (int)row))) {
-                                    int N = b.grid[j][i];
-                                    if(N == find){
-                                       cnt++;
-                                    }
-                                 }
-                              }
-                        }
-                    }
-                     b.grid[row_unk][col_unk] = cnt+'0'; 
+            if(b.grid[row][col] == cell_val){
+                     cnt = moore_count(width, height, row, col, find, b);
+                     b.grid[row][col] = cnt+'0'; 
                      cnt = 0;
               }
           }
@@ -211,7 +197,7 @@ for(unsigned row = 0; row < height; row++){
 return b;
 }
 
-unsigned moore_count_rule_2(unsigned width, unsigned height, unsigned row, unsigned col, char find, board b)
+unsigned moore_count(unsigned width, unsigned height, unsigned row, unsigned col, char find, board b)
 {
 unsigned cnt= 0;
             
@@ -252,13 +238,146 @@ void test(void)
 {   
    board b;
    char inp[MAXSQ*MAXSQ+1];
+    
+// NUM_CHAR
+   
+   // length = width * height 
+   assert(num_char(2, 2, 4)==0); 
+   
+   // length != width * height
+   assert(num_char(3, 3, 4)==1); 
 
-   // Unknowns
-   strcpy(inp, "XXX?XX535XX303X?535XXX??X");
+// CHAR_SET
+
+   //non-'X' letters
+   strcpy(inp, "Q1ra1bl0X"); 
+   assert(char_set(9, inp)==1);
+
+   //non-'?' punct
+   strcpy(inp, "0)1@1./0X"); 
+   assert(char_set(9, inp)==1);
+
+   //correct chars in string
+   strcpy(inp, "?X012345678");
+   assert(char_set(11, inp)==0); 
+
+   //space in string
+   strcpy(inp, " ");
+   assert(char_set(1, inp)==1); 
+   
+   //null char in string
+   strcpy(inp, "\0");
+   assert(char_set(1, inp)==1); 
+   
+   //'9' in string
+   strcpy(inp, "?X0123456789");
+   assert(char_set(12, inp)==1); 
+
+// NUM_MINES_STR
+
+   //num mines in string == totmines
+   strcpy(inp, "11214510X");
+   assert(num_mines_str(9, 1, inp)==0);
+
+   //num mines in string < totmines
+   strcpy(inp, "XX?10X");
+   assert(num_mines_str(6, 4, inp)==0);
+
+   //num mines in string > totmines
+   strcpy(inp, "1X1X1110X?");
+   assert(num_mines_str(10, 2, inp)==1);     
+   
+// NUM_MINES_BOARD
+
+   // num mines in board == totmines (>0)
+   strcpy(inp, "XXXXXX535XX303XX535XXXXXX");
    assert(syntax_check(16, 5, 5, inp)==true);
    b = make_board(16, 5, 5, inp);
    board2str(inp, b);
-   assert(strcmp(inp, "XXX?XX535XX303X?535XXX??X")==0);   
+   assert(num_mines_board(5, 5, b)==16);
+   
+   // num mines in board == totmines (0)
+   strcpy(inp, "0?0123435?03031?03543218?");
+   assert(syntax_check(0, 5, 5, inp)==true);
+   b = make_board(0, 5, 5, inp);
+   board2str(inp, b);
+   assert(num_mines_board(5, 5, b)==0);
+   
+// RULE_1_BOARD
+
+   // Rule 1 : 4x3 with 6 mines, 2 unknowns
+   strcpy(inp, "X?XX?XX0X123");
+   assert(syntax_check(6, 4, 3, inp)==true);
+   b = make_board(6, 4, 3, inp);
+   b = rule_1_board(4, 3, UNK, MINE, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "X4XX3XX0X123")==0);
+
+   // Rule 1 : 2x5 with 3 mines, 3 unknowns
+   strcpy(inp, "0?X2?X?0X1");
+   assert(syntax_check(3, 2, 5, inp)==true);
+   b = make_board(3, 2, 5, inp);
+   b = rule_1_board(2, 5, UNK, MINE, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "01X22X20X1")==0);
+
+   // Rule 1 : 1x6 with 2 mines, 2 unknowns
+   strcpy(inp, "0?X2?X");
+   assert(syntax_check(2, 1, 6, inp)==true);
+   b = make_board(2, 1, 6, inp);
+   b = rule_1_board(1, 6, UNK, MINE, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "01X21X")==0);
+
+   // Rule 1 : 4x2 with 0 mines, 0 unknowns
+   strcpy(inp, "01234567");
+   assert(syntax_check(0, 4, 2, inp)==true);
+   b = make_board(0, 4, 2, inp);
+   b = rule_1_board(4, 2, UNK, MINE, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "01234567")==0);
+   
+   // Rule 1 : 4x2 with 1 mine, 0 unknowns
+   strcpy(inp, "012345X7");
+   assert(syntax_check(1, 4, 2, inp)==true);
+   b = make_board(1, 4, 2, inp);
+   b = rule_1_board(4, 2, UNK, MINE, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "012345X7")==0);
+   
+// MOORE_COUNT
+ 
+   // moore_count '?'
+   strcpy(inp, "0111013X311XXX113?3101110");
+   assert(syntax_check(4, 5, 5, inp)==true);
+   b = make_board(4, 5, 5, inp);
+   assert(moore_count(5, 5, 3, 1, '?', b)==1);
+
+   // moore_count '?' edge 
+   strcpy(inp, "X13X1X1?02?1");
+   assert(syntax_check(3, 3, 4, inp)==true);
+   b = make_board(3, 3, 4, inp);
+   assert(moore_count(3, 4, 3, 2, '?', b)==2);
+ 
+   // moore_count 'X' 
+   strcpy(inp, "0111013X311XXX113?3101110");
+   assert(syntax_check(4, 5, 5, inp)==true);
+   b = make_board(4, 5, 5, inp);
+   assert(moore_count(5, 5, 3, 1, 'X', b)==2);
+   
+   //moore_count 'X' edge 1
+   strcpy(inp, "0111013X311XXX113?3101110");
+   assert(syntax_check(4, 5, 5, inp)==true);
+   b = make_board(4, 5, 5, inp);
+   assert(moore_count(5, 5, 0, 0, 'X', b)==0);
+   
+   //moore_count 'X' edge 2
+   strcpy(inp, "0111013X311XXX113?3101110");
+   assert(syntax_check(4, 5, 5, inp)==true);
+   b = make_board(4, 5, 5, inp);
+   assert(moore_count(5, 5, 0, 2, 'X', b)==1);
+
+// SYNTAX_CHECK
 
    //too big
    strcpy(inp, "11?010?011"); 
@@ -288,72 +407,49 @@ void test(void)
    strcpy(inp, "\0"); 
    assert(syntax_check(1, 1, 1, inp)==false);
 
-   // length = width * height 
-   assert(num_char(2, 2, 4)==0); 
-   // length != width * height
-   assert(num_char(3, 3, 4)==1); 
+// BOARD_2_STR & MAKE_BOARD
 
-   //non-X letter
-   strcpy(inp, "Q11111l0X"); 
-   assert(char_set(9, inp)==1);
-
-   //correct chars in string
-   strcpy(inp, "???X11110");
-   assert(char_set(9, inp)==0); 
-
-   //space in string
-   strcpy(inp, " ");
-   assert(char_set(1, inp)==1); 
-   
-   //null char in string
-   strcpy(inp, "\0");
-   assert(char_set(1, inp)==1); 
-
-   //num mines in string == totmines
-   strcpy(inp, "11111110X");
-   assert(num_mines_str(9, 1, inp)==0);
-
-   //num mines in string < totmines
-   strcpy(inp, "1X1X1110X");
-   assert(num_mines_str(9, 4, inp)==0);
-
-   //num mines in string > totmines
-   strcpy(inp, "1X1X1110X");
-   assert(num_mines_str(9, 2, inp)==1);     
-
-   // num mines in board
-   strcpy(inp, "XXXXXX535XX303XX535XXXXXX");
+   // Unknowns
+   strcpy(inp, "XXX?XX535XX303X?535XXX??X");
    assert(syntax_check(16, 5, 5, inp)==true);
    b = make_board(16, 5, 5, inp);
-   b = solve_board(b);
    board2str(inp, b);
-   assert(num_mines_b(5, 5, b)==16);
+   assert(strcmp(inp, "XXX?XX535XX303X?535XXX??X")==0);   
 
-   // moore_count_rule_2 '?' 
-   strcpy(inp, "0111013X311XXX113?3101110");
-   assert(syntax_check(4, 5, 5, inp)==true);
-   b = make_board(4, 5, 5, inp);
-   assert(moore_count_rule_2(5, 5, 3, 1, '?', b)==1);
+   // Unknowns and no mines
+   strcpy(inp, "012?34535673038?535012??0");
+   assert(syntax_check(0, 5, 5, inp)==true);
+   b = make_board(0, 5, 5, inp);
+   board2str(inp, b);
+   assert(strcmp(inp, "012?34535673038?535012??0")==0);   
  
-   // moore_count_rule_2 'X' 
-   strcpy(inp, "0111013X311XXX113?3101110");
-   assert(syntax_check(4, 5, 5, inp)==true);
-   b = make_board(4, 5, 5, inp);
-   assert(moore_count_rule_2(5, 5, 3, 1, 'X', b)==2);
-   
-   //moore_count_rule_2 'X' edge
-   strcpy(inp, "0111013X311XXX113?3101110");
-   assert(syntax_check(4, 5, 5, inp)==true);
-   b = make_board(4, 5, 5, inp);
-   assert(moore_count_rule_2(5, 5, 0, 0, 'X', b)==0);
-   
-    //moore_count_rule_2 'X' edge
-   strcpy(inp, "0111013X311XXX113?3101110");
-   assert(syntax_check(4, 5, 5, inp)==true);
-   b = make_board(4, 5, 5, inp);
-   //printf("%i\n", moore_count_rule_2(5, 5, 0, 2, 'X', b));
-   assert(moore_count_rule_2(5, 5, 0, 2, 'X', b)==1);
+   // Mines and no unknowns
+   strcpy(inp, "XXX1XX535XX303X0535XXX23X");
+   assert(syntax_check(16, 5, 5, inp)==true);
+   b = make_board(16, 5, 5, inp);
+   board2str(inp, b);
+   assert(strcmp(inp, "XXX1XX535XX303X0535XXX23X")==0); 
 
+// APPLY_RULE_2
+
+   // Rule 2 one unknown
+   strcpy(inp, "0111013X311XXX113?3101110");
+   assert(syntax_check(4, 5, 5, inp)==true);
+   b = make_board(4, 5, 5, inp);
+   b = apply_rule_2(5, 5, 4, 2, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "0111013X311XXX113X3101110")==0); 
+
+   // Rule 2 multiple unknowns
+   strcpy(inp, "2?X12?32112X");
+   assert(syntax_check(4, 4, 3, inp)==true);
+   b = make_board(4, 4, 3, inp);
+   b = apply_rule_2(4, 3, 1, 0, b);
+   board2str(inp, b);
+   assert(strcmp(inp, "2XX12X32112X")==0); 
+
+// SOLVE_BOARD 
+  
    // Rule 2 : 5x5 with 4 mines, 1 unknown 
    strcpy(inp, "0111013X311XXX113?3101110");
    assert(syntax_check(4, 5, 5, inp)==true);
@@ -369,8 +465,13 @@ void test(void)
    b = solve_board(b);
    board2str(inp, b);
    assert(strcmp(inp, "111001X21012X21012X100111")==0);
- 
-   //add lots more testing and take out any redundant tests
- 
+   
+   // Rules 1&2 : 3x4 with 3 mines, multiple unknowns
+   strcpy(inp, "1X223?X111?0");
+   assert(syntax_check(3, 3, 4, inp)==true);
+   b = make_board(3, 3, 4, inp);
+   b = solve_board(b);
+   board2str(inp, b);
+   assert(strcmp(inp, "1X223XX11110")==0);
 }
 
